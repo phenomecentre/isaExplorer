de74de2d56c32cb1244429ecf561e4f7e0334b0e
Dr Noureddin Sadawi <n.sadawi@gmail.com>
2018-03-16 17:26:57 +0000

    added more useful funs to retrieve and add assays/studies

--- isaExplorer/isaExplorer.py
+++ isaExplorer/isaExplorer.py
@@ -10,6 +10,7 @@ def exploreISA(pathToISATABFile, verbose=True):
         :param pathToISATABFile: The path to the ISATAB file.
         :type xpathToISATABFile: str
         :param verbose: Whether (or not) to print out details of Studies and Assays (default: True)
+        :type verbose: boolean
         :raise FileNotFoundError: If pathToISATABFile does not contain file 'i_Investigation.txt'.
         """
     found = False
@@ -30,3 +31,91 @@ def exploreISA(pathToISATABFile, verbose=True):
     except FileNotFoundError as err:
         raise err
     return found
+
+
+def getISAAassay(assay_num, study_num, path_to_isa_file):
+    """
+    This function returns an Assay object given the assay and study numbers in an ISA file
+    Typically, you should use the exploreISA function to check the contents 
+    of the ISA file and retrieve the assay and study numbers you are interested in!
+    :param assay_num: The Assay number (notice it's not zero-based index).
+    :type assay_num: int
+    :param study_num: The Study number (notice it's not zero-based index).
+    :type study_num: int
+    :param path_to_isa_file: The path to the ISATAB file
+    :type path_to_isa_file: str
+    :raise FileNotFoundError: If pathToISATABFile does not contain file 'i_Investigation.txt'.
+    """
+    from isatools import isatab
+    try:
+        isa = isatab.load(path_to_isa_file)
+        std = isa.studies[study_num - 1]
+        return std.assays[assay_num - 1]
+    except FileNotFoundError as err:
+        raise err
+
+
+def getISAStudy(study_num, path_to_isa_file):
+    """
+    This function returns a Study object given the study number in an ISA file
+    Typically, you should use the exploreISA function to check the contents 
+    of the ISA file and retrieve the study number you are interested in!
+    :param study_num: The Study number (notice it's not zero-based index).
+    :type study_num: int
+    :param path_to_isa_file: The path to the ISATAB file
+    :type path_to_isa_file: str
+    :raise FileNotFoundError: If pathToISATABFile does not contain file 'i_Investigation.txt'.
+    """
+    from isatools import isatab
+    try:
+        isa = isatab.load(path_to_isa_file)
+        return isa.studies[study_num - 1]
+    except FileNotFoundError as err:
+        raise err
+
+def appendStudytoISA(study, path_to_isa_file):
+    """
+    This function appends a Study object to an ISA file
+    Typically, you should use the exploreISA function to check the contents 
+    of the ISA file!
+    :param study: The Study object.
+    :type study: ISA Study object
+    :param path_to_isa_file: The path to the ISATAB file
+    :type path_to_isa_file: string
+    :raise FileNotFoundError: If pathToISATABFile does not contain file 'i_Investigation.txt'.
+    """
+    from isatools import isatab
+    try:
+        isa = isatab.load(path_to_isa_file)
+        isa.studies.append(study)
+        return True
+    except FileNotFoundError as err:
+        raise err
+    return False
+
+
+def appendAssayToStudy(assay, study_num, path_to_isa_file):
+    """
+    This function appends an Assay object to a study in an ISA file
+    Typically, you should use the exploreISA function to check the contents 
+    of the ISA file and retrieve the assay and study number you are interested in!
+    :param assay: The Assay 
+    :type assay_num: ISA Assay object
+    :param study_num: The Study number (notice it's not zero-based index).
+    :type study_num: int
+    :param path_to_isa_file: The path to the ISATAB file
+    :type path_to_isa_file: string
+    :raise FileNotFoundError: If pathToISATABFile does not contain file 'i_Investigation.txt'.
+    """
+    from isatools import isatab
+    try:
+        isa = isatab.load(path_to_isa_file)
+        std = isa.studies[study_num - 1]
+        std.assays.append(assay)
+        return True
+    except FileNotFoundError as err:
+        raise err
+    return False
+
+
+
