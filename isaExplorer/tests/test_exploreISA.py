8a0ae0e81d7f23a10a751ab120f3bcaa5e04c2ff
Noureddin Sadawi <n.sadawi@imperial.ac.uk>
2018-05-22 14:33:07 +0100

    fixed unittests .. now all pass

--- isaExplorer/tests/test_exploreISA.py
+++ isaExplorer/tests/test_exploreISA.py
@@ -6,7 +6,7 @@ import isaExplorer
 class TestExploreISA(TestCase):
     def test_exploreISA(self):
         pathToISATABFile = os.path.join(os.path.dirname(__file__), './test_data/MTBLS1/')
-        self.assertTrue(isaExplorer.exploreISA(pathToISATABFile, verbose=False))
+        self.assertIsNone(isaExplorer.exploreISA(pathToISATABFile, verbose=False))
 
 class TestGetISAAssay(TestCase):
     def test_getISAAssay(self):
@@ -22,20 +22,21 @@ class TestAppendStudytoISA(TestCase):
     def test_AppendStudytoISA(self):
         pathToISATABFile = os.path.join(os.path.dirname(__file__), './test_data/MTBLS1/')
         study = isaExplorer.isaExplorer.getISAStudy(1,pathToISATABFile)
-        self.assertTrue(isaExplorer.isaExplorer.appendStudytoISA(study,pathToISATABFile))
+        study_appended = isaExplorer.isaExplorer.appendStudytoISA(study,pathToISATABFile)
+        self.assertIsNone(study_appended)
 
 class TestDropStudyFromISA(TestCase):
     def test_dropStudyFromISA(self):
         pathToISATABFile = os.path.join(os.path.dirname(__file__), './test_data/MTBLS1/')
-        self.assertTrue(isaExplorer.isaExplorer.dropStudyFromISA(2,pathToISATABFile))
+        self.assertIsNone(isaExplorer.isaExplorer.dropStudyFromISA(2,pathToISATABFile))
 
 class TestAppendAssayToStudy(TestCase):
     def test_appendAssayToStudy(self):
         pathToISATABFile = os.path.join(os.path.dirname(__file__), './test_data/MTBLS1/')
         assay = isaExplorer.isaExplorer.getISAAssay(1,1,pathToISATABFile)
-        self.assertTrue(isaExplorer.isaExplorer.appendAssayToStudy(assay,1,pathToISATABFile))
+        self.assertIsNone(isaExplorer.isaExplorer.appendAssayToStudy(assay,1,pathToISATABFile))
 
 class TestDropAssayFromStudy(TestCase):
     def test_dropAssayFromStudy(self):
         pathToISATABFile = os.path.join(os.path.dirname(__file__), './test_data/MTBLS1/')
-        self.assertTrue(isaExplorer.isaExplorer.dropAssayFromStudy(2,1,pathToISATABFile))
+        self.assertIsNone(isaExplorer.isaExplorer.dropAssayFromStudy(2,1,pathToISATABFile))
