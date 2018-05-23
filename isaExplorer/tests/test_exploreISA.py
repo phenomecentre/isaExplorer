76470eef0b77ff2fd1a83bb5a88d65a2fef7dab3
Jake TM Pearce <jake.pearce+git@gmail.com>
2018-05-23 10:13:53 +0100

    Properly encapsulate test writes with `tempfile`

--- isaExplorer/tests/test_exploreISA.py
+++ isaExplorer/tests/test_exploreISA.py
@@ -1,41 +1,58 @@
 from unittest import TestCase
 import os
+import tempfile
+from distutils import dir_util
 
 import isaExplorer
 
+pathToISATABFile = os.path.join(os.path.dirname(__file__), 'test_data', 'MTBLS1')
+
 class TestExploreISA(TestCase):
     def test_exploreISA(self):
-        pathToISATABFile = os.path.join(os.path.dirname(__file__), 'test_data', 'MTBLS1')
         self.assertIsNone(isaExplorer.exploreISA(pathToISATABFile, verbose=False))
 
 class TestGetISAAssay(TestCase):
     def test_getISAAssay(self):
-        pathToISATABFile = os.path.join(os.path.dirname(__file__), 'test_data', 'MTBLS1')
         self.assertTrue(isaExplorer.isaExplorer.getISAAssay(1,1,pathToISATABFile))
 
 class TestGetISAStudy(TestCase):
     def test_getISAStudy(self):
-        pathToISATABFile = os.path.join(os.path.dirname(__file__), 'test_data', 'MTBLS1')
         self.assertTrue(isaExplorer.isaExplorer.getISAStudy(1,pathToISATABFile))
 
-class TestAppendStudytoISA(TestCase):
+class Test_study_operations(TestCase):
     def test_AppendStudytoISA(self):
-        pathToISATABFile = os.path.join(os.path.dirname(__file__), 'test_data', 'MTBLS1')
-        study = isaExplorer.isaExplorer.getISAStudy(1,pathToISATABFile)
-        self.assertIsNone(isaExplorer.isaExplorer.appendStudytoISA(study,pathToISATABFile))
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            pathToTempISATABFile = os.path.join(tmpdirname, 'MTBLS1')
+            dir_util.copy_tree(pathToISATABFile, pathToTempISATABFile)
 
-class TestDropStudyFromISA(TestCase):
+            study = isaExplorer.isaExplorer.getISAStudy(1,pathToTempISATABFile)
+            self.assertIsNone(isaExplorer.isaExplorer.appendStudytoISA(study,pathToTempISATABFile))
+
     def test_dropStudyFromISA(self):
-        pathToISATABFile = os.path.join(os.path.dirname(__file__), 'test_data', 'MTBLS1')
-        self.assertIsNone(isaExplorer.isaExplorer.dropStudyFromISA(2,pathToISATABFile))
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            pathToTempISATABFile = os.path.join(tmpdirname, 'MTBLS1')
+            dir_util.copy_tree(pathToISATABFile, pathToTempISATABFile)
 
-class TestAppendAssayToStudy(TestCase):
+            study = isaExplorer.isaExplorer.getISAStudy(1,pathToTempISATABFile)
+            isaExplorer.isaExplorer.appendStudytoISA(study,pathToTempISATABFile)
+
+            self.assertIsNone(isaExplorer.isaExplorer.dropStudyFromISA(2,pathToTempISATABFile))
+
+class Test_assay_operations(TestCase):
     def test_appendAssayToStudy(self):
-        pathToISATABFile = os.path.join(os.path.dirname(__file__), 'test_data', 'MTBLS1')
-        assay = isaExplorer.isaExplorer.getISAAssay(1,1,pathToISATABFile)
-        self.assertIsNone(isaExplorer.isaExplorer.appendAssayToStudy(assay,1,pathToISATABFile))
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            pathToTempISATABFile = os.path.join(tmpdirname, 'MTBLS1')
+            dir_util.copy_tree(pathToISATABFile, pathToTempISATABFile)
 
-class TestDropAssayFromStudy(TestCase):
+            assay = isaExplorer.isaExplorer.getISAAssay(1,1,pathToTempISATABFile)
+            self.assertIsNone(isaExplorer.isaExplorer.appendAssayToStudy(assay,1,pathToTempISATABFile))
+
     def test_dropAssayFromStudy(self):
-        pathToISATABFile = os.path.join(os.path.dirname(__file__), 'test_data', 'MTBLS1')
-        self.assertIsNone(isaExplorer.isaExplorer.dropAssayFromStudy(2,1,pathToISATABFile))
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            pathToTempISATABFile = os.path.join(tmpdirname, 'MTBLS1')
+            dir_util.copy_tree(pathToISATABFile, pathToTempISATABFile)
+
+            assay = isaExplorer.isaExplorer.getISAAssay(1,1,pathToTempISATABFile)
+            isaExplorer.isaExplorer.appendAssayToStudy(assay,1,pathToTempISATABFile)
+
+            self.assertIsNone(isaExplorer.isaExplorer.dropAssayFromStudy(2,1,pathToTempISATABFile))
