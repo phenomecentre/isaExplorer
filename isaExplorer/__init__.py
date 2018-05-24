2fc817d53b6acba46918a0db8dd2f72e5e1d785e
Jake TM Pearce <jake.pearce+git@gmail.com>
2018-05-24 15:35:23 +0100

    Bump verision no to next alpha

--- isaExplorer/__init__.py
+++ isaExplorer/__init__.py
@@ -1,6 +1,6 @@
 from .isaExplorer import exploreISA, getISAAssay, getISAStudy, appendStudytoISA
 from .isaExplorer import appendAssayToStudy, dropAssayFromStudy, dropStudyFromISA
 
-__version__ = '0.1.0'
+__version__ = '0.1.1a'
 
 __all__ = ['exploreISA', 'getISAAssay', 'getISAStudy', 'appendStudytoISA', 'appendAssayToStudy', 'dropAssayFromStudy', 'dropStudyFromISA']
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='isaExplorer',
-    version='0.1.0',
+    version='0.1.1a',
     description='Explore and manipulate the contents of ISATAB files',
     url='http://github.com/phenomecentre/isaexplorer',
     author='Noureddin Sadawi',
@@ -24,17 +24,16 @@ setup(name='isaExplorer',
         ---------------------------------------------
 
         .. image:: https://travis-ci.org/phenomecentre/isaExplorer.svg?branch=master
-        :target: https://travis-ci.org/phenomecentre/isaExplorer
-        :alt: Travis CI build status
+           :target: https://travis-ci.org/phenomecentre/isaExplorer
+           :alt: Travis CI build status
 
         .. image:: https://codecov.io/gh/phenomecentre/isaExplorer/branch/master/graph/badge.svg
-        :target: https://codecov.io/gh/phenomecentre/isaExplorer
-        :alt: Test coverage
+           :target: https://codecov.io/gh/phenomecentre/isaExplorer
+           :alt: Test coverage
 
         |
 
         Tools for querying `ISA-TAB <http://isa-tools.org>`_ study description files.
-
         """,
     zip_safe=True
     )
